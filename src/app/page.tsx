86d5d294bbea7c39f0d4ee4467e7566ee8cef394
Sumato Ken <bermime1@gmail.com>
2023-02-16 09:47:01 +0100

    added info form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,25 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import { ChangeEvent, MouseEvent, useState } from "react";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
+  const [form, setForm] = useState({
+    fullname: "",
+    code: "",
+    branch: "",
+    email: "",
+    level: "",
+  });
+  useEffect(() => {
+    console.log(form);
+
+    return () => {};
+  }, [form]);
+
   const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const fileInput = e.target;
     console.log(fileInput.files);
@@ -57,10 +70,30 @@ export default function Home() {
         return;
       }
 
-      console.log("File was uploaded successfylly:", data);
+      sendFormData();
     } catch (error) {
       console.error(error);
       alert("Sorry! something went wrong.");
+    }
+  };
+  const sendFormData = async () => {
+    const res = await fetch("/api/form", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+    const {
+      data,
+      error,
+    }: {
+      data: {
+        url: string | string[];
+      } | null;
+      error: string | null;
+    } = await res.json();
+
+    if (error || !data) {
+      alert(error || "Sorry! something went wrong.");
+      return;
     }
   };
   return (
@@ -70,8 +103,92 @@ export default function Home() {
         <span className="text-blue-600 dark:text-blue-500">the world's #1</span>{" "}
         CRM.
       </h1>
-      <div className="w-1/4">
+      <div className="w-full md:w-1/4">
         <form className="w-full p-3" onSubmit={(e) => e.preventDefault()}>
+          <div className="grid gap-6 mb-6 ">
+            <div>
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Nom Complet
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John Crow"
+                required
+                onChange={(e) => setForm({ ...form, fullname: e.target.value })}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="code apogée"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Code Appogée
+              </label>
+              <input
+                type="number"
+                id="code-appogée"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="15806341"
+                required
+                onChange={(e) => setForm({ ...form, code: e.target.value })}
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="company"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Filiére
+              </label>
+              <select
+                id="branches"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                onChange={(e) => setForm({ ...form, branch: e.target.value })}
+              >
+                <option selected>Choisiser une filiére</option>
+                <option value="SMP">SMP</option>
+                <option value="SMC">SMC</option>
+                <option value="SVI">SVI</option>
+                <option value="STU">STU</option>
+                <option value="SMA">SMA</option>
+                <option value="SMI">SMI</option>
+              </select>
+            </div>
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="john.doe@company.com"
+              required
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+          <select
+            id="level"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => setForm({ ...form, level: e.target.value })}
+          >
+            <option selected>Selectionner votre niveau</option>
+            <option value="A1">A1</option>
+            <option value="A2">A2</option>
+            <option value="B1">B1</option>
+            <option value="B2">B2</option>
+            <option value="C1">C1</option>
+            <option value="C2">C2</option>
+          </select>
           <label
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             htmlFor="large_size"
---
+++ src/pages/api/form.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma";
+import { Student } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<{
+    data: Student | null;
+    error: string | null;
+  }>
+) => {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    res.status(405).json({
+      data: null,
+      error: "Method Not Allowed",
+    });
+    return;
+  }
+  const body = JSON.parse(req.body);
+  try {
+    const student = await prisma.student.create({
+      data: {
+        fullname: body.fullname,
+        email: body.email,
+        code: body.code,
+        branch: body.branch,
+      },
+    });
+    if (student) {
+      res.status(200).json({
+        data: student,
+        error: null,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ data: null, error: "Internal Server Error" });
+  }
+  console.log(body);
+};
+export default handler;
