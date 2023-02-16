5307bd3de77cf3facff76c45226f8a2d806f8ca9
Sumato Ken <bermime1@gmail.com>
2023-02-16 11:35:31 +0100

    fixed typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -348,7 +348,7 @@ export default function Home() {
               className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white"
               htmlFor="large_size"
             >
-              Large file input
+              Certificat
             </label>
             <input
               className="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
